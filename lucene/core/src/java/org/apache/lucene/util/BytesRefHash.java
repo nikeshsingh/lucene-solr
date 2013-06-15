48838678a99700b53fb58b8813612218c8cdca09
Simon Willnauer <simonw@apache.org>
2013-06-15 20:33:42 +0200

    revert BytesRefHash

--- lucene/core/src/java/org/apache/lucene/util/BytesRefHash.java
+++ lucene/core/src/java/org/apache/lucene/util/BytesRefHash.java
@@ -59,7 +59,6 @@ public final class BytesRefHash {
   private int count;
   private int lastCount = -1;
   private int[] ids;
-  private int[] hashes;
   private final BytesStartArray bytesStartArray;
   private Counter bytesUsed;
 
@@ -89,7 +88,6 @@ public final class BytesRefHash {
     this.pool = pool;
     ids = new int[hashSize];
     Arrays.fill(ids, -1);
-    hashes = new int[hashSize];
     this.bytesStartArray = bytesStartArray;
     bytesStart = bytesStartArray.init();
     bytesUsed = bytesStartArray.bytesUsed() == null? Counter.newCounter() : bytesStartArray.bytesUsed();
